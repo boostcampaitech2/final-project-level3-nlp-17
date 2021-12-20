f57dbdc57862c31619caaddfe6b6f43a3a087e76
Kyungwon oh <kyungwon.dev@gmail.com>
2021-12-20 18:02:21 +0900

    frontend ver 2.0

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -1,3 +1,5 @@
+import json
+
 from starlette.responses import RedirectResponse
 from fastapi import FastAPI, Form, Request
 from fastapi.templating import Jinja2Templates
@@ -24,9 +26,7 @@ async def get_index_page(request: Request):
 
 @app.post("/result", response_class=HTMLResponse)
 async def post_result(request: Request, userid: str = Form(...)):
-    print(userid)
     userinfo = getSpectatorInfo2(userid)
-    print(userinfo)
     if userinfo == False:
         return RedirectResponse(url="/", status_code=302)
     if 'gameId' not in userinfo:
@@ -36,28 +36,34 @@ async def post_result(request: Request, userid: str = Form(...)):
     data = {}
     red_team = {}
     blue_team = {}
-    lines = ['TOP', 'JUNGLE', 'MIDDLE', 'BOTTOM', 'UTILITY']
+    lines = ["TOP", "JUNGLE", "MIDDLE", "BOTTOM", "UTILITY"]
     team = {100: [], 200: []}
+    user_color = 0
     # pprint(userinfo)
     for participant in userinfo['participants']:
         team[participant['teamId']].append(participant)
+        if participant['summonerName'] == userid:
+            user_color = int((participant['teamId'] - 100) / 100)
 
+    print(user_color)
     for teamId in team.keys():
         for participant, line in zip(team[teamId], lines):
-            key = '_'.join([str(teamId), str(line)])
-            data[key] = [participant['summonerId'], participant['championId']]
-            champ = df.loc[df['key'] == str(participant['championId'])].iloc[0]
+            key = "_".join([str(teamId), str(line)])
+            data[key] = [participant["summonerId"].replace("'", '"'), participant["championId"]]
+            champ = df.loc[df["key"] == str(participant["championId"])].iloc[0]
+
             if teamId == 100: # 파랑색
-                blue_team[key] = [champ[ 'name'], champ['image'], str(line), participant['summonerName']]
+                blue_team[key] = [champ['name'], champ['image'], str(line), participant['summonerName']]
             else: # 붉은색
                 red_team[key] = [champ['name'], champ['image'], str(line), participant['summonerName']]
 
-    data = {"summonerDict": data, 'matchId': 'KR_' + str(userinfo['gameId'])}
+    data = json.dumps({"summonerDict": data, "matchId": "KR_" + str(userinfo['gameId'])})
     predict = requests.post("http://101.101.216.156:6013/model/inference/by-matchInfo", data=data).json()
-    #print(predict)
-    #pprint(team)
     # predict = 0.7
-    return templates.TemplateResponse('index.html', context={'request': request, 'predict': predict, 'red_team': red_team , 'blue_team' : blue_team})
+    print(predict)
+    return templates.TemplateResponse('index.html', context={
+        'request': request, 'predict': predict['output'][0][user_color],
+        'red_team': red_team, 'blue_team': blue_team, "user_color": user_color})
 
 
 @app.exception_handler(StarletteHTTPException)
