4ce92d8b5a7fde058100c6f44d23b6788e2cb830
geup <knmj789@gmail.com>
2021-12-22 18:30:17 +0900

    bugfix

--- code/inference.py
+++ code/inference.py
@@ -46,67 +46,26 @@ if __name__=='__main__':
     data_files = {"test": "test.csv"}
     dataset = load_dataset("PDJ107/riot-data", data_files=data_files, revision='cgm_20', use_auth_token=True)
 
-    train_dataset = TabularDatasetFromHuggingface(dataset['test'])
+    dataset = TabularDatasetFromHuggingface(dataset['test'])
 
     softmax = nn.Softmax(dim=-1)
 
     t = 0
     f = 0
-    i = 0
-    j = 0
 
-    inference_df = pd.DataFrame(columns=['id','target'])
-    for i, x in enumerate(dataset):
+    for x, label in tqdm(dataset):
         output, _ = model(x.view(1,-1).to(device))
         p_output = softmax(output.detach().cpu())
         max_p = torch.max(p_output)
         
         output = torch.argmax(output, dim=1)
 
-        inference_df = inference_df.append(pd.Series([i+1, output.item()], index = inference_df.columns), ignore_index=True)
-
-        #print(f'({p_output[0][0].item():.2f}, {p_output[0][1].item():.2f})', 'label : ',label.item())
-        # if f >= 10:
-        #     break
-
-        # if output != label:
-            
-        #     f += 1
-        #     #print('---------False-----------')
-        #     #print(f'({p_output[0][0].item():.2f}, {p_output[0][1].item():.2f})', 'label : ',label.item())
-        #     # if (max_p>=0.999) and (j==0):
-        #     #     print('---------False-----------')
-        #     #     print(f'({p_output[0][0].item():.2f}, {p_output[0][1].item():.2f})', 'label : ',label.item())
-        #     #     false_x = x.detach().cpu()
-        #     #     j+=1
-        #     #     if i+j == 2:
-        #     #         break
-
-            
-        # else:
-        #     t += 1
-        #     #print('---------True-----------')
-        #     #print(f'({p_output[0][0].item():.2f}, {p_output[0][1].item():.2f})', 'label : ',label.item())
-        #     # if (max_p>=0.999) and (i==0):
-        #     #     print('---------True-----------')
-        #     #     print(f'({p_output[0][0].item():.2f}, {p_output[0][1].item():.2f})', 'label : ',label.item())
-        #     #     true_x = x.detach().cpu()
-        #     #     i+=1
-        #     #     if i+j == 2:
-        #     #         break
-    
-    
-    inference_df.to_csv('submission.csv', index = False)
-    #print('test accuracy : ', t/(t+f+1e-10))
-    # false_x = torch.stack([false_x[i*35:(i+1)*35] for i in range(10)], dim=0)
-
-    # false_df = pd.DataFrame(false_x.numpy(), columns=dataset.columns.split(',')[:35])
-    # false_df.to_csv('./false_data.csv')
-
-    # true_x = torch.stack([true_x[i*35:(i+1)*35] for i in range(10)], dim=0)
-
-    # true_df = pd.DataFrame(true_x.numpy(), columns=dataset.columns.split(',')[:35])
-    # true_df.to_csv('./true_data.csv')
+        if output != label:
+            f += 1
+        else:
+            t += 1
+        
+    print('test accuracy : ', t/(t+f+1e-10))
 
 
 
